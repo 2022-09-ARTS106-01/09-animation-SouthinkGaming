a9e381896549a0c3b94340acb67f7e87e638cb3f
SouthinkGaming <southink419@gmail.com>
2022-11-29 15:30:47 -0500

    update
    3:30pm
    11/29/2022

---
+++ Assets/Scenes/Player_Controler.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+    private Animator animator;
+    
+    // This for initialization
+    void Start() {
+        animator = this.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+        var vertical = Input.GetAxis("Vertical");
+        var horizontal = Input.GetAxis("Horizontal");
+
+        if (vertical > 0) {
+            animator.SetInteger("Direction", 2);
+        } else if (vertical < 0) {
+            animator.SetInteger("Direction", 0);
+        } else if (horizontal > 0) {
+            animator.SetInteger("Direction", 1);
+        } else if (horizontal < 0) {
+            animator.SetInteger("Direction", 3);
+        }
+    }
+}
