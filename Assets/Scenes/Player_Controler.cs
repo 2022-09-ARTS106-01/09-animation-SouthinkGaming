71ca0d925a7c2b392374c6cae5a1a3e42b7835fd
SouthinkGaming <southink419@gmail.com>
2022-11-30 11:09:49 -0500

    Update to player controler

--- Assets/Scenes/Player_Controler.cs
+++ Assets/Scenes/Player_Controler.cs
@@ -2,39 +2,39 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class playerController : MonoBehaviour
-{
+public class playerController : MonoBehaviour {
 
     private Animator animator;
 
     // This is for initialization
-    void Start()
-    {
+    void Start() {
+
         animator = this.GetComponent<Animator>();
+    
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
 
         var vertical = Input.GetAxis("Vertical");
         var horizontal = Input.GetAxis("Horizontal");
 
-        if (vertical > 0)
-        {
+        if (vertical > 0) {
+
             animator.SetInteger("Direction", 2);
-        }
-        else if (vertical < 0)
-        {
+        
+        } else if (vertical < 0) {
+
             animator.SetInteger("Direction", 0);
-        }
-        else if (horizontal > 0)
-        {
+
+        } else if (horizontal > 0) {
+            
             animator.SetInteger("Direction", 1);
-        }
-        else if (horizontal < 0)
-        {
+        
+        } else if (horizontal < 0) {
+
             animator.SetInteger("Direction", 3);
+
         }
     }
 }
