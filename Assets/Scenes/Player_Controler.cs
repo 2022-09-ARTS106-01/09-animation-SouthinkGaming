8552c691e2e9a332bf05223ccd801e32f4162be5
SouthinkGaming <southink419@gmail.com>
2022-11-30 12:21:29 -0500

    update

--- Assets/Scenes/Player_Controler.cs
+++ Assets/Scenes/Player_Controler.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class playerController : MonoBehaviour {
+public class Player_Controler : MonoBehaviour {
 
     private Animator animator;
 
